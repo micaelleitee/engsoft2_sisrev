e79fd325d07986064ff7c11a1c8924942eb1abab
Micael Leite <micaelleite01@gmail.com>
2025-10-21 22:02:57 -0300

    feat: Adicionar integração NativeWind com suporte Tailwind CSS e atualizar a configuração do projeto

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function index() {
+    return (
+        <View className='bg-red-500 justify-center items-center h-full'>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
