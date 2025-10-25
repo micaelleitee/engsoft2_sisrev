3b538759acced1f98c8afcde37d5698e0f96d7a4
Micael Leite <micaelleite01@gmail.com>
2025-10-25 15:18:45 -0300

    feat: Add logo image to login screen and update layout

--- app/login/login.tsx
+++ app/login/login.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 
 export default function Login() {
     return (
         <View className='justify-center items-center h-full'>
-            
+                <Image source={require('../../src/img/LogoIF.png')} className=' w-15 h-15'/>
+                
         </View>
     )
 }
